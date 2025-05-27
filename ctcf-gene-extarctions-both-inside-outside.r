ad18935ac2bc28e4a1dd3cb4c321ea8bcd6221c6
Amarinder Singh Thind <thind.amarinder@gmail.com>
2025-05-27 12:16:01 +1000

    Update ctcf-gene-extarctions-both-inside-outside.r

--- ctcf-gene-extarctions-both-inside-outside.r
+++ ctcf-gene-extarctions-both-inside-outside.r
@@ -1,40 +1,16 @@
----
-title: "ctcf loop genes extraction both inside and outside loop upto 1000bp either side with ensemble-id"
-author: "Amarinder Thind"
-date: "`r Sys.Date()`"
-output:
-  html_document: default
-  pdf_document: default
----
+# Title: "ctcf loop genes extraction both inside and outside loop upto 1000bp either side with ensemble-id"
+#author: "Amarinder Thind"
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+setwd('Path')
 
-
-```{r path-setup}
-
-setwd('/home/singh/Dropbox/Amarinder/Amarinder_main_projects/CTCF_motif_AT/2024-ctcf-dragen-additional-samples/Step1.b-extraction-of-loops-gene-list/')
-
-```
-
-```{r}
-
-## may update this with supplimentry file data #GSE63525_NHEK_HiCCUPS_looplist_with_motifs 2
-## This data has information of motif sequence, can be used to study the patterns of motif changes
-## HOWVER, MAKE SURE THEY ARE FROM HG38 REF GENOME, OTHERWISE LIFTOVER
-
-motif_1806_hg38 <- read.delim("/home/singh/Dropbox/Amarinder/Amarinder_main_projects/CTCF_motif_AT/2024-ctcf-dragen-additional-samples/motifs_with_loops_sorted_hg38_1806.bed", header=FALSE)
+## Load CTCFbs motif bed file with 3' and 5' Anchor 
+motif_1806_hg38 <- read.delim("motifs_with_loops_sorted_hg38_1806.bed", header=FALSE)
 
 # In here, the first side is end of the motif and second side is starting of the motif ## since we captured inside part only 
-```
-
-
-```{r}
 # Add additional 1000 bp on left side of first motif (of Loop XX) and 1000 bp on right side of second motif ( of Loop Xx)
 
+#step1 # separate the 5 prime-motif site from 3 prime-motif (will end up in 903/1806) entries for each
 
-#step1 # separate the 5prime-motif site from 3primemotif (will end up in 903/1806) entries for each
 CTCF_5site <- motif_1806_hg38[motif_1806_hg38$V5 =="5_CTCF",] 
 CTCF_3site <- motif_1806_hg38[motif_1806_hg38$V5 =="3_CTCF",]
 
@@ -44,10 +20,6 @@ CTCF_5site$end_1000 <- CTCF_5site$V2
 CTCF_3site$start_1000 <- CTCF_3site$V3 
 CTCF_3site$end_1000 <- CTCF_3site$V3 + 1000
  
-```
-
-
-```{r}
 # Merge the two data frames based on column V6
 merged_df <- merge(CTCF_5site[, c("V1",   "V6","V5", "start_1000")], 
                    CTCF_3site[, c("V1",   "V6", "V5", "end_1000")], 
@@ -56,32 +28,21 @@ merged_df <- merge(CTCF_5site[, c("V1",   "V6","V5", "start_1000")],
 # View the merged data frame
 head(merged_df)
 
-
-```
-
-
-
-```{r}
 library(biomaRt)
 
 
 # Connect to the Ensembl database
 mart <- useMart("ensembl")
-
 mart <- useDataset("hsapiens_gene_ensembl", mart)
 
 ##confirm loaded reference is hg38 or not?
 listDatasets(mart)
-
 listAttributes(mart)
 
 # Define the attributes and filters
 attributes <- c("chromosome_name", "start_position", "end_position", "strand","ensembl_gene_id", "hgnc_symbol")
 filters <- c("chromosome_name", "start", "end")
-```
 
-
-```{r}
 # List of genomic regions
 regions_5 <- merged_df[,c(2,4,7,1)]
 
@@ -100,16 +61,12 @@ regions_list5 <- apply(regions_5, 1, function(row) {
 
 #write.csv(regions_5,"modified-Co-ordinates-loop_with-1000k.csv")
  
-```
 
-
-
-
-```{r}
- library(biomaRt)
+library(biomaRt)
 library(parallel)
 
 # Function to query BioMart and add identifiers
+
 get_data_with_identifier <- function(region, mart, attributes, filters) {
   values <- list(
     chromosome_name = region$chromosome_name,
@@ -150,15 +107,9 @@ parallel_get_data <- function(regions_list, mart, attributes, filters) {
   return(all.genes)
 }
 
-
-
 # Query BioMart for each region in parallel and combine results
 all.genes_5 <- parallel_get_data(regions_list5, mart, attributes, filters)
 
-```
-
-
-```{r}
 library(dplyr)
 
 # Group by 'identifier' and summarize other columns
@@ -174,11 +125,6 @@ aggregated_data_5 <- all.genes_5 %>%
     .groups = 'drop'
   )
 
-```
-
-
-
-```{r}
 # Clean function for NA values
 
 # Clean function
@@ -206,9 +152,6 @@ df_cleaned <- aggregated_data_5 %>%
     hgnc_symbol = clean_column(hgnc_symbol)
   )
 
-```
-
-```{r}
 # ### We lost few chromesom/start/end entries inthe beiging of region file, We can fill this from previous one
 # ## Ideally, we can skip these cols from most of the cleaning
 # 
@@ -218,7 +161,6 @@ df_cleaned <- aggregated_data_5 %>%
 # df_cleaned2 <- full_join( regions_5, df_cleaned[,c("ensembl_gene_id","hgnc_symbol","identifier") ], by = "identifier", suffix = c(".df1", ".df2"))
 
 write.csv(df_cleaned,"Loops_with_gene_list_combine-inside-and-1000bp-outside-.csv")
-```
 
 
 
